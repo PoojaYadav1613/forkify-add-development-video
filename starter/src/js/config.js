b0a3768549381a5b7263375a115f4ff6f725d522
PoojaYadav1613 <poojawhale1613@gmail.com>
2023-06-03 13:46:49 +0530

    Initial commit

---
+++ starter/src/js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 100;
+
+export const RES_PER_PAGE = 10;
+export const KEY = 'e3c15969-a25b-491b-b8a1-320fdb069871';
+export const MODAL_CLOSE_SEC = 2.5;
